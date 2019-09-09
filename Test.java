5903fda20f541bf0727cfcef0c65c14aae2a5300
TongYm <969191140@qq.com>
2019-09-09 20:11:37 +0800

    created a new sayhello class

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test{
+   public static void main(String[] args){
+       System.out.println("Below is the function of hello");
+	   sayhello();
+	   }
+	   public static void sayhello(){
+		   System.out.println("say Hello!");
+	   }
+}
